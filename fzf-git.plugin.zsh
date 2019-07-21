f68073dce987f0a616e7e133d27a23aa2af14c10
hschne <hans.schnedlitz@gmail.com>
2019-07-21 12:28:05 +0200

    Add more stuff

--- fzf-git.plugin.zsh
+++ fzf-git.plugin.zsh
@@ -15,50 +15,98 @@ hash diff-so-fancy &>/dev/null && fzf_git_fancy='|diff-so-fancy'
 # https://github.com/wfxr/emoji-cli
 hash emojify &>/dev/null && fzf_git_emojify='|emojify'
 
+fzf-git::checkout_file() {
+  fzf-git::inside_work_tree || { fzf-git::info "Not a git repository"; return 1; }
+
+  # If an argument is provided call the command, otherwise call completion
+  [[ "$#" -ge 1 ]] && { eval $cmd "$@"; return "$?"; }
+  local cmd files opts
+  cmd="git diff --color=always -- {} $fzf_git_emojify $fzf_git_fancy"
+  opts="
+        $FZF_GIT_DEFAULT_OPTS
+        -m -0 --preview=\"$cmd\"
+    "
+  files="$(fzf-git::list_staged | FZF_DEFAULT_OPTS="$opts" fzf)"
+  [[ -n "$files" ]] && echo "$files" |xargs -I{} git checkout {} && git status --short && return
+}
+
+fzf-git::complete_all_branches() {
+  fzf-git::inside_work_tree || { fzf-git::info "Not a git repository"; return 1; }
+
+  local opts="$FZF_GIT_DEFAULT_OPTS"
+
+  _fzf_complete "$opts" "$@" < <(fzf-git::list_all_branches)
+}
+
+fzf-git::complete_local_branches() {
+  fzf-git::inside_work_tree || { fzf-git::info "Not a git repository"; return 1; }
+
+  local opts="$FZF_GIT_DEFAULT_OPTS"
+
+  _fzf_complete "$opts" "$@" < <(fzf-git::list_local_branches)
+}
+
+fzf-git::complete_staged_files() {
+  fzf-git::inside_work_tree || { fzf-git::info "Not a git repository"; return 1; }
+
+  # TODO: Figure out a way to do preview here as well?
+  local opts="$FZF_GIT_DEFAULT_OPTS"
+
+  _fzf_complete "$opts" "$@" < <(fzf-git::list_staged_files)
+}
+
+
 # Takes a command to execute and a completion
 # 
 # If the whole thing was called with additional arguments, just call the original
 # cmd. Otherwise invoke the passed completion to get the argument.
 fzf-git::cmd_proxy() {
   fzf-git::inside_work_tree || { fzf-git::info "Not a git repository"; return 1; }
-  
 
   local cmd=$1
-  shift
+  local completion=$2
+  shift 2
   # If an argument is provided call the command, otherwise call completion
   [[ "$#" -ge 1 ]] && { eval $cmd "$@"; return "$?"; }
 
   local opts="$FZF_GIT_DEFAULT_OPTS"
-  branch=$(fzf-git::list_branches | \
+  branch=$(fzf-git::list_$completion | \
     FZF_DEFAULT_OPTS="$opts" fzf )
   [[ -n "$branch" ]] && eval $cmd "$branch"
 }
 
-fzf-git::complete_branch() {
-  fzf-git::inside_work_tree || { fzf-git::info "Not a git repository"; return 1; }
 
-  local opts="$FZF_GIT_DEFAULT_OPTS"
-
-  _fzf_complete "$opts" "$@" < <(fzf-git::list_branches)
-}
-
-fzf-git::list_branches() {
+fzf-git::list_all_branches() {
   git branch -a | \
     grep -vw 'HEAD' | sort | \
     sed 's/^..//' | cut -d' ' -f1 | \
     sed 's|remotes\/.*\/||' | uniq | sort
 }
 
-fzf-git::setup_completions() {
-  for cmd in ${FZF_GIT_BRANCH_COMPLETIONS[@]}; do
-    eval "_fzf_complete_$cmd() { fzf-git::complete_branch \$@ }"
-  done
+fzf-git::list_local_branches() {
+  git branch | \
+    grep -vw 'HEAD' | sort | \
+    sed 's/^..//' | cut -d' ' -f1 
 }
 
-alias gcb='fzf-git::cmd_proxy "git checkout"'
-alias gcob='fzf-git::cmd_proxy "git checkout"'
-alias gbd='fzf-git::cmd_proxy "git branch -d"'
+fzf-git::list_staged_files() {
+  git ls-files --modified "$(git rev-parse --show-toplevel)"
+}
 
-FZF_GIT_BRANCH_COMPLETIONS=(gcb gbd)
+fzf-git::setup_completions() {
+  eval "_fzf_complete_gco() { fzf-git::complete_staged_files \$@ }"
+  eval "_fzf_complete_gcof() { fzf-git::complete_staged_files \$@ }"
+  eval "_fzf_complete_gcob() { fzf-git::complete_all_branches \$@ }"
+  eval "_fzf_complete_gbd() { fzf-git::complete_local_branches \$@ }"
+  # for cmd in ${FZF_GIT_BRANCH_COMPLETIONS[@]}; do
+  #   eval "_fzf_complete_$cmd() { fzf-git::complete_branch \$@ }"
+  # done
+}
+
+
+alias gco='fzf-git::checkout_file'
+alias gcof='fzf-git::checkout_file'
+alias gcob='fzf-git::cmd_proxy "git checkout" all_branches'
+alias gbd='fzf-git::cmd_proxy "git branch -d" local_branches'
 
 fzf-git::setup_completions
